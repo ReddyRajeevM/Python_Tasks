1eb6518600f0b4db8249ca66ced01aa227ba2e98
ReddyRajeevM <rajeev.warior2@gmail.com>
2025-04-29 19:09:49 +0530

    Create Check_if a Number is Even or Odd.py

---
+++ Check_if a Number is Even or Odd.py
@@ -0,0 +1,6 @@
+#Task 1: Check_if a Number is Even or Odd
+number=int(input("Enter a number: "))
+if number % 2 == 0:
+    print(str(number)+" is an Even number")
+else:
+    print(str(number)+" is an Odd number")
