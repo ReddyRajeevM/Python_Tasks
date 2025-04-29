3c2e92210130c0794ed8b38c49565fddfc1fc7de
ReddyRajeevM <rajeev.warior2@gmail.com>
2025-04-29 19:07:50 +0530

    Create Create_a_Personalized_Greeting.py

---
+++ Create_a_Personalized_Greeting.py
@@ -0,0 +1,6 @@
+#Task 2: Create a Personalized Greeting
+
+FirstName= input("Enter your First name: ")
+LastName= input("Enter your Last name: ")
+Name=FirstName+LastName
+print("Hello, "+Name+"! Welcome to the python program")
