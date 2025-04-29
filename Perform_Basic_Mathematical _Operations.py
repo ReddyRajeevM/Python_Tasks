ff63874ebae581fab27ae5520df0e3eea81a0822
ReddyRajeevM <rajeev.warior2@gmail.com>
2025-04-29 19:04:40 +0530

    Update and rename my_first_program.py to Perform_Basic_Mathematical _Operations.py
     Basic Python Concepts

---
+++ Perform_Basic_Mathematical _Operations.py
@@ -0,0 +1,7 @@
+# Task 1: Perform Basic Mathematical Operations
+num1=int(input("enter number1: "))
+num2=int(input("enter number2: "))
+print('Subraction: ', num1-num2)
+print('Addition: ', num1+num2)
+print('Multiplication: ',num1*num2)
+print('Division: ',num1/num2)
