ffd7aa589be786654d386bb6d576b0c5f6a327a3
ReddyRajeevM <rajeev.warior2@gmail.com>
2025-05-12 12:45:29 +0530

    Update Read a File and Handle Errors.py

--- Assignment_4/Read a File and Handle Errors.py
+++ Assignment_4/Read a File and Handle Errors.py
@@ -1,1 +1,12 @@
+# Task 1: Read a File and Handle Errors 
+def openfile(filename):
+     try:
+          with open(filename,'r') as file1:
+               final_output=file1.readlines()
+               for i in final_output:
+                     print(i)
 
+     except FileNotFoundError:
+          print("The file "+filename+" not found.")
+
+print(openfile("sample.txt"))
